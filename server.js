0ee0089c84eb000b1fb6f3810a051a51028ffec4
david tuan <super496@hotmail.com>
2016-05-06 06:44:24 +0000

    init socket.io on client end

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var express = require("express");
 var PORT = process.env.PORT || 8080;
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(){
+    console.log('User connected via socket.io!');
+});
 
 http.listen(PORT, function() {
     console.log('server listening to port: ' + PORT);
